9b5b14e294bd5cd5ab53a2ccc0bf3d8b07392c91
sachinmanidvsverewrvwDECS <90509273+sachinmanidvsverewrvwDECS@users.noreply.github.com>
2021-12-24 20:09:32 -0800

    Add files via upload

---
+++ navigation/TabNavigator.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+//import Ionicons from 'react-native-vector-icons/Ionicons';
+import CreatePost from '../screens/CreatePost';
+import Feed from '../screens/Feed';
